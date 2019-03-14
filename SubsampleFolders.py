f71b78415fc0b6b9bd806d6a6ddd6b2119c73cf0
Pat Lorch <pat_lorch@yahoo.com>
2019-03-14 10:31:55 -0400

    add output_dir2 and df

--- SubsampleFolders.py
+++ SubsampleFolders.py
@@ -1,16 +1,19 @@
 import os
 import shutil
 import random
+import pandas as pd
 
 """ Create training data set by subsampling folders. """
 
 low_number = 1000
-random_amount = 1000
-# Need to add ability to output a manifest of paths of chosen images so that they can be 
-#  removed to make training and testing subsets.
+random_amount = 2000
 
-root_dir = r'E:\UNPROCESSED\train1'
-output_dir = r'E:\UNPROCESSED\train1_1000_test'
+root_dir = r'E:\UNPROCESSED\train1_1000_Deer_Coyote'
+output_dir = r'E:\UNPROCESSED\train1_1000_test_3rd'
+output_dir2 = r'E:\UNPROCESSED\train1_manifests'
+outfilename = 'train1_1000_3rd_DEER_manifest.csv'
+df = pd.DataFrame(columns=["Subdir","Filepath","File","Destpath"])
+
 
 for folder in os.listdir(root_dir):
 
@@ -23,6 +26,7 @@ for folder in os.listdir(root_dir):
 	if len(files) < low_number:
 		for file in files:
 			shutil.copyfile(os.path.join(src, file), os.path.join(dst,file))
+			df = df.append({"Subdir": folder, "Filepath": os.path.join(src, file), "File": file, "Destpath": os.path.join(dst,file)}, ignore_index=True)
 	else:
 		# Amount of random files you'd like to select
 		# Use xrange if running with python 2 (e.g., py -2 SubsampleFolders.py)
@@ -35,3 +39,6 @@ for folder in os.listdir(root_dir):
 				random.seed() # to get same results, enter an integer here
 				file = random.choice(files)
 				shutil.copyfile(os.path.join(src, file), os.path.join(dst,file))
+				df = df.append({"Subdir": folder, "Filepath": os.path.join(src, file), "File": file, "Destpath": os.path.join(dst,file)}, ignore_index=True)
+df_outpath = os.path.join(output_dir2, outfilename)
+df.to_csv(df_outpath, mode='w', index=False, header=True)
